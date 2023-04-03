74e71896e48f3cc15daafb23d6b89ef4f30c0c87
jostpas1 <32129279+jostpas1@users.noreply.github.com>
2023-04-03 16:00:06 +0200

    create_tables

---
+++ WebScraping/CREATE TABLE.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Resultat (
+    Spieltag_Datum date,
+    ID_TeamH INTEGER,
+    ID_TeamG INTEGER,
+    Anzahl_Tore INTEGER,
+    Anzahl_Schuesse INTEGER,
+    Ballbesitz_TeamH REAL,
+    Ballbesitz_TeamG REAL,
+    Anzahl_SchuesseH INTEGER,
+    Anzahl_SchuesseG INTEGER,    
+    Tore_TeamH INTEGER,
+    Tore_TeamG INTEGER,
+    Fouls_TeamH INTEGER,
+    Fouls_TeamG INTEGER,
+    Ecken_TeamH INTEGER,
+    Ecken_TeamG INTEGER,
+    Offsides_TeamH INTEGER,
+    Offsides_TeamG INTEGER
+);
+
+CREATE TABLE Mannschaften (
+    Mannschaft VARCHAR(255),
+    ID INTEGER,
+    Punkte INTEGER,
+    Tore INTEGER,
+    Rang INTEGER
+);

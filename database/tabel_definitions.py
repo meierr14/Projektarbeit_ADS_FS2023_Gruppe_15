865fb54595cf195ba7db553cfd7090e82c5edde4
Renzo Meier <meierr14@students.zhaw.ch>
2023-05-01 16:29:34 +0200

    Erstellung der Tabellen alle über gliches File update_tabels.py kann nun durchgeführt werden ohne doppelte Einträge
    Entfernung von Code welches tabellen erstellt aus anderen files

--- database/tabel_definitions.py
+++ database/tabel_definitions.py
@@ -1,5 +1,6 @@
 create_table_resultate = """
-    CREATE TABLE resultate(
+    CREATE TABLE IF NOT EXISTS bundesliga_resultate(
+        Match_ID INTEGER PRIMARY KEY,
         Spieltag_Datum DATE,
         ID_TeamH INTEGER,
         ID_TeamG INTEGER,
@@ -17,15 +18,42 @@ create_table_resultate = """
         Ecken_TeamG INTEGER,
         Offsides_TeamH INTEGER,
         Offsides_TeamG INTEGER
-    )
+    );
 """
 
 create_table_mannschaften = """
-    CREATE TABLE mannschaften(
+    CREATE TABLE IF NOT EXISTS bundesliga_mannschaften(
+        Team_ID INTEGER PRIMARY KEY,
         Mannschaft VARCHAR(255),
-        ID INTEGER,
         Punkte INTEGER,
         Tore INTEGER,
         Rang INTEGER
-    )
+    );
 """
+
+create_table_pass_stats = """
+    CREATE TABLE IF NOT EXISTS bundesliga_pass_stats (
+        id SERIAL PRIMARY KEY,
+        matchday INTEGER NOT NULL,
+        team_name TEXT NOT NULL,
+        pass_percentage NUMERIC NOT NULL
+    );
+"""
+
+create_table_tore_schuesse = '''
+    CREATE TABLE IF NOT EXISTS bundesliga_shots_stats (
+        id SERIAL PRIMARY KEY,
+        matchday INTEGER NOT NULL,
+        team_name TEXT NOT NULL,
+        value NUMERIC NOT NULL
+    );
+'''
+
+create_table_zweikaempfe = '''
+    CREATE TABLE IF NOT EXISTS bundesliga_duels_stats (
+        id SERIAL PRIMARY KEY,
+        matchday INTEGER NOT NULL,
+        team_name TEXT NOT NULL,
+        value NUMERIC NOT NULL
+    );
+'''
---
+++ database/update_tables.py
@@ -0,0 +1,68 @@
+import psycopg2
+import json
+
+
+def table_insert_mannschaften(data_mannschaften):
+    # Abafüllen der Tabelle Mannschaften
+    for row in data_mannschaften:
+        name = row['teamName']
+        id = row['teamId']
+        cur.execute("INSERT INTO bundesliga_mannschaften (team_id, mannschaft) SELECT %s, %s WHERE NOT EXISTS (SELECT 1 FROM bundesliga_mannschaften WHERE team_id=%s);", (id, name, id))
+
+    conn.commit()
+
+
+def table_update_mannschaften(data_tabelle):
+    for row in data_tabelle:
+        id = row['teamInfoId']
+        points = row['points']
+        goals = row['goals']
+        cur.execute(f'UPDATE bundesliga_mannschaften SET punkte = {points}, tore = {goals} WHERE team_id = {id}')
+        
+    conn.commit()
+
+
+def table_insert_result(data_spiele):
+    for row in data_spiele:
+        match_id = row['matchID']
+        datum = row['matchDateTime']
+        team_heim = row['team1']['teamId']
+        team_gast = row['team2']['teamId']
+        goals_heim = row['matchResults'][0]['pointsTeam1']
+        goals_gast = row['matchResults'][0]['pointsTeam2']
+        goals_total = goals_heim + goals_gast
+        cur.execute("INSERT INTO bundesliga_resultate (id, spieltag_datum, id_teamh, id_teamg, anzahl_tore, tore_teamh, tore_teamg) SELECT %s, %s, %s, %s, %s, %s, %s WHERE NOT EXISTS (SELECT 1 FROM bundesliga_resultate WHERE id=%s);", (match_id, datum, team_heim, team_gast, goals_total, goals_heim, goals_gast, match_id))  
+    
+    conn.commit()
+
+
+# Verbindung zur Datenbank herstellen
+conn = psycopg2.connect(
+    host="localhost",
+    database="PSQL_ADSFS2023Gruppe15",
+    user="ADSFS2023Gruppe15",
+    password="ADS_FS_2023_G15!?"
+)
+
+# Cursor erstellen
+cur = conn.cursor()
+
+with open('APIs/JSONData/mannschaften.json', 'r') as file:
+    data_mannschaften = json.load(file)
+
+with open('APIs/JSONData/tabelle.json', 'r') as file:
+    data_tabelle = json.load(file)
+
+with open('APIs/JSONData/ganze_saison.json', 'r') as file:
+    data_spiele = json.load(file)
+
+
+
+table_insert_mannschaften(data_mannschaften)
+table_update_mannschaften(data_tabelle)
+table_insert_result(data_spiele)
+
+
+#Verbindung schließen
+cur.close()
+conn.close()

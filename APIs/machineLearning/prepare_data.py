5edcd579439c2d829c2ebf1f8f770ab9fd0a0507
Renzo Meier <meierr14@students.zhaw.ch>
2023-05-08 16:14:04 +0200

    Erstellung von Trainings- und Testset für das machine learning model

--- APIs/machineLearning/prepare_data.py
+++ APIs/machineLearning/prepare_data.py
@@ -1,5 +1,6 @@
 import psycopg2
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # Verbindung zur Datenbank herstellen
 conn = psycopg2.connect(
@@ -9,18 +10,21 @@ conn = psycopg2.connect(
     password="ADS_FS_2023_G15!?"
 )
 
+# SQL Queries für die DB Abfrage
 query_tabelle = "SELECT team_id, mannschaft, punkte, tore FROM bundesliga_mannschaften"
 query_resultate = "SELECT matchday, id_teamh, id_teamg, tore_teamh, tore_teamg FROM bundesliga_resultate"
 query_shots_stats = "SELECT matchday, team_id, shots_total FROM bundesliga_shots_stats"
 query_duel_stats = "SELECT matchday, team_id, duels_total, duels_won FROM bundesliga_duels_stats"
 query_pass_stats = "SELECT matchday, team_id, pass_complete, pass_failed, pass_total, pass_percentage FROM bundesliga_pass_stats"
 
+# Daten aus DB Tabellen lesen
 df_tabelle = pd.read_sql(query_tabelle, conn)
 df_resultate = pd.read_sql(query_resultate, conn)
 df_shots = pd.read_sql(query_shots_stats, conn)
 df_duels = pd.read_sql(query_duel_stats, conn)
 df_pass = pd.read_sql(query_pass_stats, conn)
 
+# Zusammenfassen aller Daten in ein einziges DataFrame
 merged_df = pd.merge(df_resultate, df_tabelle, left_on='id_teamh', right_on='team_id')
 merged_df = pd.merge(merged_df, df_tabelle, left_on='id_teamg', right_on='team_id', suffixes=('_home', '_guest'))
 merged_df = pd.merge(merged_df, df_shots, left_on=['id_teamh', 'matchday'], right_on=['team_id', 'matchday'])
@@ -31,6 +35,15 @@ merged_df = pd.merge(merged_df, df_pass, left_on=['id_teamh', 'matchday'], right
 merged_df = pd.merge(merged_df, df_pass, left_on=['id_teamg', 'matchday'], right_on=['team_id', 'matchday'], suffixes=('_home', '_guest'))
 
 
+merged_df_dummies = pd.get_dummies(merged_df, columns=['mannschaft_home', 'mannschaft_guest'])
 
-print(merged_df.head())
+# Variablen definieren
+X = merged_df_dummies.drop(['id_teamh', 'id_teamg', 'tore_teamh', 'tore_teamg', 'matchday'], axis=1)
+y = merged_df_dummies[['tore_teamh', 'tore_teamg']]
+
+# Daten in Trainings- und Testset splitten
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+
+print(merged_df_dummies.head())
 
